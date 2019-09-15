e8a0a590ad851b4928bca0eba0db572496b4e72e
Raymundo Valdez García <ing.ray.valdez@gmail.com>
2019-09-15 01:34:02 -0500

    Start our refactoring.2

--- src/vehicles2/CreateATruck.java
+++ src/vehicles2/CreateATruck.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vehicles;
+package vehicles2;
 
 import java.awt.HeadlessException;
 import javax.swing.JOptionPane;
